ce5d897b87917323134432bbfdbfa72a9f5e35bf
Nick Spencer <strangebrew202@gmail.com>
2023-09-11 09:14:55 -0600

    Fix: typo in Item model found building model tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :supermarket
   has_many :customers_items
-  has_many :customers, trough: :customers_items
+  has_many :customers, through: :customers_items
 end
