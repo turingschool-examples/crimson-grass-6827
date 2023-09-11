0c19a22941f8d8ae30d0fceb25ea2b0a75c0c981
Nick Spencer <strangebrew202@gmail.com>
2023-09-11 09:45:51 -0600

    Add: Base show page for /customer/:id, customers controller, #find_market_by_item method to Supermarket model

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,3 +1,7 @@
 class Supermarket < ApplicationRecord
   has_many :items
+
+  def self.find_market_by_item(item)
+    find(item.supermarket_id)
+  end
 end
--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe "Customer show", type: :feature do
       @customer_1 = Customer.create!(name: "Willy")
       @kings = Supermarket.create!(name: "King Supers", location: "Fort Collins")
       @tomato = @kings.items.create!(name: "Tomato", price: 1)
+      CustomersItem.create!(customer_id: @customer_1.id, item_id: @tomato.id)
     end
 
     it "I see the customer's name, And i see a list of it's items
   including the item's name, price, and the name of the supermarket that it belongs to" do
     
     visit "/customers/#{@customer_1.id}"
+    save_and_open_page
 
     expect(page).to have_content(@customer_1.name)
     expect(page).to have_content(@tomato.name)
