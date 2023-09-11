33cf18a5bf1b666ac6ed5901cdbf3f0cce131037
Cory Powell <coryrpowell@gmail.com>
2023-09-11 12:20:42 -0400

    Initial commit and creation of customer model and customer_item joins table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CustomerItem.destroy_all
+Customer.destroy_all
+Item.destroy_all
+Supermarket.destroy_all
+
+#supermarket
+@supermarket_1 = Supermarket.create!(name: "Kroger", location: "555 Food Ave")
+
+#customer
+@customer_1 = Customer.create!(name: "Fred")
+
+#item
+@item_1 = @supermarket_1.items.create!(name: "Eggs", price: 6)
+@item_2 = @supermarket_1.items.create!(name: "Banana", price: 1)
+@item_3 = @supermarket_1.items.create!(name: "Milk", price: 4)
+
+#customer item
+@eggs = CustomerItem.create!(customer: @customer_1, item: @item_1)
+@banana = CustomerItem.create!(customer: @customer_1, item: @item_2)
+@milk = CustomerItem.create!(customer: @customer_1, item: @item_3)
---
+++ spec/features/customers/show_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe "customers#show" do
+  before(:each) do
+    @supermarket_1 = Supermarket.create!(name: "Kroger", location: "555 Food Ave")
+
+    @customer_1 = Customer.create!(name: "Fred")
+
+    @item_1 = @supermarket_1.items.create!(name: "Eggs", price: 6)
+    @item_2 = @supermarket_1.items.create!(name: "Banana", price: 1)
+    @item_3 = @supermarket_1.items.create!(name: "Milk", price: 4)
+
+    @eggs = CustomerItem.create!(customer: @customer_1, item: @item_1)
+    @banana = CustomerItem.create!(customer: @customer_1, item: @item_2)
+    @milk = CustomerItem.create!(customer: @customer_1, item: @item_3)
+  end
+
+  #   Story 1
+
+  # As a visitor, 
+  # When I visit a customer show page,
+  # I see the customer's name,
+  # And I see its a list of its items
+  # including the item's name, price, and the name of the supermarket that it belongs to.
+
+  describe "display customer info" do
+    it "shows the customer's name" do
+      visit "/customers/#{customer.id}"
+
+      expect(page).to have_content("Name: #{@customer_1.name}")
+    end
+
+    it "shows the customer's items including the item's name, price, and the name of the supermarket that it belongs to" do
+      visit "/customers/#{customer.id}"
+
+
+      expect(page).to have_content("Name: #{@item_1.name}")
+      expect(page).to have_content("Price: #{@item_1.price}")
+      expect(page).to have_content("Supermarket: #{@item_1.supermarket_id}")
+    end
+  end
+
+
+end
