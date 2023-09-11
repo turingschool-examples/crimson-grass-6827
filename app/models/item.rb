0df2198fe9afc31135ce17081cf92280cdff7e50
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-09-11 09:44:20 -0600

    test: complete setup of test files, create migrations for the customer table and customeritems table, update modelsfor validations and relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true, numericality: true
+
+
   belongs_to :supermarket
+  has_many :customer_items
+  has_many :customers, through: :customer_items
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :supermarket }
+    it { should have_many :customers }
+  end
+
+  describe "relationships" do 
+    it { should validate_presence_of (:name) }
+    it { should validate_presence_of (:price) }
   end
 end
