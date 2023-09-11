681748531020767d91aeb30ae7761de3a086c91b
Lane Bretschneider <lanebret85@gmail.com>
2023-09-11 09:26:21 -0700

    feat: add peaches item record to seeds file so I can add it to a customer it isnt already associated with

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ sprouts = Supermarket.create!(name: "Sprouts", location: "123 Food St.")
 bananas = sprouts.items.create!(name: "Bananas", price: 1)
 apples = sprouts.items.create!(name: "Apples", price: 2)
 oranges = sprouts.items.create!(name: "Oranges", price: 3)
+peaches = sprouts.items.create!(name: "Peaches", price: 4)
+
 
 CustomerItem.create!(customer_id: michael_phelps.id, item_id: bananas.id)
 CustomerItem.create!(customer_id: michael_phelps.id, item_id: apples.id)
