5fec79f8f2d9f0a447efa71117c823fe80b5d291
Lane Bretschneider <lanebret85@gmail.com>
2023-09-11 09:27:27 -0700

    feat: add update action to controller and add code to view to display form to add an item to a customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,9 @@ class CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
+
+  def update
+    CustomerItem.create!(customer_id: params[:id], item_id: params[:item_id])
+    redirect_to "/customers/#{params[:id]}"
+  end
 end
