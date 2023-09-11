2a6a20ce39fd36592dd896bb073016a96f3744b3
Nick Spencer <strangebrew202@gmail.com>
2023-09-11 09:17:36 -0600

    Add: model tests for Many to Many relationship and spec files for customers, customers_item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :supermarket }
     it { should have_many :customers_items}
+    it {should have_many(:customers).through(:customers_items)}
   end
 end
