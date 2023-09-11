7432033b986dfbfb9010edcd16c278e7b0365ef3
Lane Bretschneider <lanebret85@gmail.com>
2023-09-11 10:41:10 -0700

    feat: add supermarket_customers method to Supermarket model class

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,3 +1,7 @@
 class Supermarket < ApplicationRecord
   has_many :items
+
+  def supermarket_customers
+    customers = Customer.joins(items: :supermarket).where("supermarkets.name = '#{self.name}'").distinct.order(:name)
+  end
 end
