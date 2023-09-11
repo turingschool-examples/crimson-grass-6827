51b490e461ba250051e548c63461b35c408eba83
Joop Stark <stark.joop@gmail.com>
2023-09-11 11:43:15 -0600

    ran out of time to do the extention

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,8 @@ class Customer < ApplicationRecord
   has_many :customer_items
   has_many :items, through: :customer_items
 
+  # def self.unique_customers_by_store(store)
+  #   joins(customer_items: {items: :supermarket}).where(:name == store)
+  # end
+
 end
