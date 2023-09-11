a133cf5a693f3b98ab8ba026b474a4c47e0f56a0
Lane Bretschneider <lanebret85@gmail.com>
2023-09-11 09:59:45 -0700

    test: add model test for customer_count method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Item, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:customers).through(:customer_items) }
   end
+
+  before :each do
+    @michael_phelps = Customer.create!(name: "Michael Phelps")
+    @serena_williams = Customer.create!(name: "Serena Williams")
+
+    @sprouts = Supermarket.create!(name: "Sprouts", location: "123 Food St.")
+
+    @apples = @sprouts.items.create!(name: "Apples", price: 2)
+
+    CustomerItem.create!(customer_id: @michael_phelps.id, item_id: @apples.id)
+    CustomerItem.create!(customer_id: @serena_williams.id, item_id: @apples.id)
+  end
+
+  describe "#customer_count" do
+    it "can count the number of customers who have bought an item" do
+      query = @apples.customer_count
+
+      expect(query).to eq(2)
+    end
+  end
 end
