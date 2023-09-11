ed1f2b2b0c3817bf09dbcba0cdaf2b8435fd9219
Dylan Timmons <dylanstimmons@icloud.com>
2023-09-11 11:43:53 -0600

    finish US 2

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe CustomersController, type: :feature do
         expect(page).to have_content(2)
         expect(page).to have_content("Kings")
     end
+
+    it 'can add an item' do
+        kings = Supermarket.create(name: 'Kings', location: '123 main st')
+        safeway = Supermarket.create(name: 'Safeway', location: '5678 water ln')
+        customer1 = Customer.create(name: 'Bob')
+        bread = Item.create(name: 'Bread', price: 3, supermarket: kings)
+        eggs = Item.create(name: 'Eggs', price: 2, supermarket: kings)
+        milk = Item.create(name: 'Milk', price: 5, supermarket: safeway)
+        CustomerItem.create(customer: customer1, item: bread)
+        CustomerItem.create(customer: customer1, item: eggs)
+
+        visit "/customers/#{customer1.id}"
+
+        fill_in(:item_id, with: milk.id)
+        click_button("Add Item")
+
+        expect(page).to have_content(milk.name)
+        expect(page).to have_content(milk.price)
+    end
 end
