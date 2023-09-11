649f13bd37126e0016c05c167885ad899b8d1568
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-11 10:55:40 -0500

    update the supermarket model spec file to make sure methods and relationships are fully tested

--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -1,7 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Supermarket, type: :model do
-  describe 'relationships' do
+  describe "relationships" do
     it { should have_many :items }
+
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
   end
 end
