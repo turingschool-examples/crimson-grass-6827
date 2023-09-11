7ba9af292edff678adc50bcc02f1c5e5edcde2f9
Antoine Aube <aaube3@gmail.com>
2023-09-11 09:28:34 -0600

    create controller files

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -1,3 +1,5 @@
 class Supermarket < ApplicationRecord
   has_many :items
+  validates :name, presence: true
+  validates :location, presence: true
 end
--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Supermarket, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :location }
+  end
 end
