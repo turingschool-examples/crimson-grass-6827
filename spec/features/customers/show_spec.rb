03b005ae67062bdca655c40b5ea2c78abf5cc2f0
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-11 11:51:16 -0500

    Create the show page and the formatting to display the information of each item in the list

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -12,7 +12,27 @@ RSpec.describe "the custmer show page" do
     expect(page).to have_content("#{customer1.name}'s Shopping List")
   end
 
-  # xit "displays the list of items they intend to buy and what supermarket they are from" do
+  it "displays the list of items they intend to buy and what supermarket they are from" do
+    market1 = Supermarket.create(name: "Supermercado", location: "123 Food St.")
+    item1 = market1.items.create(name: "Banana", price: 1)
+    item2 = market1.items.create(name: "Coffee", price: 3)
+    item3 = market1.items.create(name: "Pasta", price: 3)
+    customer1 = Customer.create(name: "Guy")
+    customer_item1 = CustomerItem.create(item: item1, customer: customer1)
+    customer_item2 = CustomerItem.create(item: item2, customer: customer1)
+    customer_item3 = CustomerItem.create(item: item3, customer: customer1)
+    visit "/customers/#{customer1.id}"
 
-  # end
+    within("div.grocery-list") do
+      expect(page).to have_content(item1.name)
+      expect(page).to have_content(item1.price)
+      # expect(page).to have_content(item1.market)
+      expect(page).to have_content(item2.name)
+      expect(page).to have_content(item2.price)
+      # expect(page).to have_content(item2.market)
+      expect(page).to have_content(item3.name)
+      expect(page).to have_content(item3.price)
+      # expect(page).to have_content(item3.market)
+    end
+  end
 end
