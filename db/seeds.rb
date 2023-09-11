40a4bbf2a5968295794057a3736ce9b2e88c6169
Lane Bretschneider <lanebret85@gmail.com>
2023-09-11 09:03:01 -0700

    feat: add records from test to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+michael_phelps = Customer.create!(name: "Michael Phelps")
+
+sprouts = Supermarket.create!(name: "Sprouts", location: "123 Food St.")
+
+bananas = sprouts.items.create!(name: "Bananas", price: 1)
+apples = sprouts.items.create!(name: "Apples", price: 2)
+oranges = sprouts.items.create!(name: "Oranges", price: 3)
+
+CustomerItem.create!(customer_id: michael_phelps.id, item_id: bananas.id)
+CustomerItem.create!(customer_id: michael_phelps.id, item_id: apples.id)
+CustomerItem.create!(customer_id: michael_phelps.id, item_id: oranges.id)
