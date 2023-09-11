69b82a14d671813ec0dbad174b5d10439d13807a
Nick Spencer <strangebrew202@gmail.com>
2023-09-11 10:05:16 -0600

    Checkpoint: US 2 spec test built, basic form built in customers/:id show page, next is route for post method and controller action to create the CustomersItem

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -1,23 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Customer show", type: :feature do
+  before(:each) do
+    @customer_1 = Customer.create!(name: "Willy")
+    @kings = Supermarket.create!(name: "King Supers", location: "Fort Collins")
+    @tomato = @kings.items.create!(name: "Tomato", price: 1)
+    @onion = @kings.items.create!(name: "onion", price: 1)
+    CustomersItem.create!(customer_id: @customer_1.id, item_id: @tomato.id)
+  end
   describe 'As a visitor' do
-    before(:each) do
-      @customer_1 = Customer.create!(name: "Willy")
-      @kings = Supermarket.create!(name: "King Supers", location: "Fort Collins")
-      @tomato = @kings.items.create!(name: "Tomato", price: 1)
-      CustomersItem.create!(customer_id: @customer_1.id, item_id: @tomato.id)
-    end
-
-    it "I see the customer's name, And i see a list of it's items
-  including the item's name, price, and the name of the supermarket that it belongs to" do
+    describe 'When I visit /customers/:id' do
+      it "I see the customer's name, And i see a list of it's items
+      including the item's name, price, and the name of the supermarket that it belongs to" do
+        
+        visit "/customers/#{@customer_1.id}"
     
-    visit "/customers/#{@customer_1.id}"
+        expect(page).to have_content(@customer_1.name)
+        within(".item_list")do
+          expect(page).to have_content(@tomato.name)
+          expect(page).to have_content(@tomato.price)
+          expect(page).to have_content(@kings.name)
+        end
+      end
 
-    expect(page).to have_content(@customer_1.name)
-    expect(page).to have_content(@tomato.name)
-    expect(page).to have_content(@tomato.price)
-    expect(page).to have_content(@kings.name)
+      it "I see a form to add an item to this customer. When I fill in a field with the ID of an existing item
+      and click submit, then I am redirected back to customer's show page and I see the new item listed" do
+
+        visit "/customers/#{@customer_1.id}"
+
+        fill_in "new_item", with: "#{@onion.id}"
+        click_on "Submit"
+
+        expect(page).to have_content(@onion.name)
+      end
     end
   end
 end
