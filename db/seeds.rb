3bac4f451c83f869edfd9aba21de7a4c123288fb
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-11 09:47:53 -0600

    feat: add some seeds to test development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+customer_1 = Customer.create!(name: "Sally Shopper")
+
+supermarket_1 = Supermarket.create!(
+  name: "Corner Market",
+  location: "123 Food Street"
+)
+
+banana = supermarket_1.items.create!(
+  name: "Banana",
+  price: 1
+)
+
+apple = supermarket_1.items.create!(
+  name: "Apple",
+  price: 2
+)
+
+CustomerItem.create!(customer: customer_1, item: banana)
+CustomerItem.create!(customer: customer_1, item: apple)
