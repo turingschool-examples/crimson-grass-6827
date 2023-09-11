258af5c1976127550f4742ff5e96a8c12915595d
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-11 11:23:26 -0600

    add seed data for more local testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+supermarket_1 = Supermarket.create!(name: "Get ya Fishes", location: "1234 Fish St")
+item_1 = supermarket_1.items.create!(name: "Tuna", price: 2)
+item_2 = supermarket_1.items.create!(name: "Red Snapper", price: 5)
+item_3 = supermarket_1.items.create!(name: "Salmon", price: 3)
+customer_1 = Customer.create!(name: "Tyler")
+customer_2 = Customer.create!(name: "Kiwi")
+customer_3 = Customer.create!(name: "Chicken")
+CustomerItem.create!(item_id: item_1.id, customer_id: customer_1.id)
+CustomerItem.create!(item_id: item_1.id, customer_id: customer_2.id)
+CustomerItem.create!(item_id: item_2.id, customer_id: customer_1.id)
