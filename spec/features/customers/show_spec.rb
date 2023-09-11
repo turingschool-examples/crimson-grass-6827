0ac8adec37229d940fd37990a94f413dfb23526f
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-09-11 09:59:19 -0600

    Set up test 1

---
+++ spec/features/customers/show_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe Customer, type: :feature do
+  before(:each) do
+    @supermarket = Supermarket.create(name: "Whole Foods", location: "124 Wall St.")
+    @customer1 = Customer.create(name: "John Dwayne")
+    @item1 = @supermarket.items.create(name: "Banana", price: 20)
+  end
+
+  it "As a visitor, When I visit a customer show page, I see the customer's name, And I see its a list of its items including the item's name, price, and the name of the supermarket that it belongs to." do
+
+    visit "/customers/#{@customer1.id}"
+
+    expect(page).to have_content(@customer1.name)
+    expect(page).to have_content(@item1.name)
+    expect(page).to have_content(@item1.price)
+    expect(page).to have_content(@supermarket.name)
+  end
+end
