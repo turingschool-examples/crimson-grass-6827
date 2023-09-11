f64577a530c584c990195f16e87b91bf696f19aa
Nick Spencer <strangebrew202@gmail.com>
2023-09-11 10:42:24 -0600

    Add: #count_purchases to CustomersItem to calculate how many times an item has been purchased

--- app/models/customers_item.rb
+++ app/models/customers_item.rb
@@ -1,4 +1,8 @@
 class CustomersItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
+
+  def self.count_purchases(item)
+    where('item_id = ?', item.id).count
+  end
 end
--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe "Items Index", type: :feature do
   before(:each) do
     @customer_1 = Customer.create!(name: "Willy")
+    @customer_2 = Customer.create!(name: "Dan")
+    @customer_3 = Customer.create!(name: "Shelly")
+    @customer_4 = Customer.create!(name: "Lily")
     @kings = Supermarket.create!(name: "King Supers", location: "Fort Collins")
     @tomato = @kings.items.create!(name: "Tomato", price: 1)
     @onion = @kings.items.create!(name: "onion", price: 1)
     CustomersItem.create!(customer_id: @customer_1.id, item_id: @tomato.id)
+    CustomersItem.create!(customer_id: @customer_2.id, item_id: @tomato.id)
+    CustomersItem.create!(customer_id: @customer_3.id, item_id: @tomato.id)
+    CustomersItem.create!(customer_id: @customer_4.id, item_id: @tomato.id)
+    CustomersItem.create!(customer_id: @customer_3.id, item_id: @onion.id)
+    CustomersItem.create!(customer_id: @customer_4.id, item_id: @onion.id)
   end
   describe "As a visitor" do
     describe "When I visit /items" do
@@ -17,14 +25,16 @@ RSpec.describe "Items Index", type: :feature do
         
         within(".#{@tomato.name}") do
           expect(page).to have_content(@tomato.name)
-          expect(page).to have_content(@tomato.price)
-          expect(page).to have_content(@tomato.supermarket.name)
+          expect(page).to have_content("Price: #{@tomato.price}")
+          expect(page).to have_content("Supermarket: #{@tomato.supermarket.name}")
+          expect(page).to have_content("Purchases: 4")
         end
 
         within(".#{@onion.name}") do
           expect(page).to have_content(@onion.name)
-          expect(page).to have_content(@onion.price)
-          expect(page).to have_content(@onion.supermarket.name)
+          expect(page).to have_content("Price: #{@onion.price}")
+          expect(page).to have_content("Supermarket: #{@onion.supermarket.name}")
+          expect(page).to have_content("Purchases: 2")
         end
       end
     end
