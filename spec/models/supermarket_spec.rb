f241fd273ea0617041cd0e0b4fbe9532e726194a
Lane Bretschneider <lanebret85@gmail.com>
2023-09-11 10:39:52 -0700

    test: add model test for supermarket_customers method

--- spec/models/supermarket_spec.rb
+++ spec/models/supermarket_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe Supermarket, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  before :each do
+    @michael_phelps = Customer.create!(name: "Michael Phelps")
+    @serena_williams = Customer.create!(name: "Serena Williams")
+
+    @sprouts = Supermarket.create!(name: "Sprouts", location: "123 Food St.")
+
+    @bananas = @sprouts.items.create!(name: "Bananas", price: 1)
+    @apples = @sprouts.items.create!(name: "Apples", price: 2)
+    @oranges = @sprouts.items.create!(name: "Oranges", price: 3)
+    @peaches = @sprouts.items.create!(name: "Peaches", price: 4)
+
+    CustomerItem.create!(customer_id: @michael_phelps.id, item_id: @bananas.id)
+    CustomerItem.create!(customer_id: @michael_phelps.id, item_id: @apples.id)
+    CustomerItem.create!(customer_id: @michael_phelps.id, item_id: @oranges.id)
+    CustomerItem.create!(customer_id: @serena_williams.id, item_id: @apples.id)
+    CustomerItem.create!(customer_id: @serena_williams.id, item_id: @oranges.id)
+    CustomerItem.create!(customer_id: @serena_williams.id, item_id: @peaches.id)
+  end
+
+  describe "#supermarket_customers" do
+    it "returns a list of unique customers who have shopped at the given supermarket" do
+      query = @sprouts.supermarket_customers
+
+      expect(query).to eq([@michael_phelps, @serena_williams])
+    end
+  end 
 end
