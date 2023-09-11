8a7b3b1db8dc3851301f789b84570a1f86ed800a
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-11 10:14:25 -0600

    feat: add logic to create action in customer items controller to create association between customer and item added

--- app/controllers/customer_items_controller.rb
+++ app/controllers/customer_items_controller.rb
@@ -1,5 +1,9 @@
 class CustomerItemsController < ApplicationController
   def create
-    
+    item = Item.find(params[:item_id])
+    customer = Customer.find(params[:customer_id])
+    CustomerItem.create!(customer: customer, item: item)
+
+    redirect_to "/customers/#{customer.id}"
   end
 end
