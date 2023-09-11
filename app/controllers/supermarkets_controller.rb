710ed8a3b74a74eefef8c7cf14003d099f92c10a
Nick Spencer <strangebrew202@gmail.com>
2023-09-11 11:55:26 -0600

    Refactor: moved #find_supermarket_shoppers call out of supermarkets_controller and into the show view in order to keep controllers thin

--- app/controllers/supermarkets_controller.rb
+++ app/controllers/supermarkets_controller.rb
@@ -1,6 +1,5 @@
 class SupermarketsController < ApplicationController
   def show
     @supermarket = Supermarket.find(params[:id])
-    @customers = Customer.find_supermarket_shoppers(@supermarket)
   end
 end
