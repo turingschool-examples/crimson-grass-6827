766e291590eebcca983bd54cbaa78bed00ab5ea2
Will Weston <Williamweston81@gmail.com>
2023-09-11 11:02:01 -0500

    User Story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@supermarket_1 = Supermarket.create(name: "Reasors", location: "Tulsa, OK")
+
+@customer_1 = Customer.create(name: "Will")
+
+@item_1 = @supermarket_1.items.create(name: "Banana", price: 1)
+@item_2 = @supermarket_1.items.create(name: "Apple", price: 1)
+
+@customer_item_1 = CustomerItem.create(customer_id: @customer_1.id, item_id: @item_1.id)
+@customer_item_2 = CustomerItem.create(customer_id: @customer_1.id, item_id: @item_2.id)
